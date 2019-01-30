1e7abd0b6e4e40d9c9ad807dc4783c1310fe93f4
Ethan Grab <ethangrab@gmail.com>
2019-01-30 16:35:16 -0700

    board class added letter converter and corresponding cell method

--- battleshipz/lib/board.rb
+++ battleshipz/lib/board.rb
@@ -1,24 +1,36 @@
+class Board
 
-
-
-def letter_converter(width, length)
-  alphabet = ("A".."Z").to_a
-  width_letter_array = (1..width).map do |num|
-    num = alphabet[num - 1]
+  def initialize
   end
-  other_array = []
-  width_letter_array.cycle(length) do |letter|
-    other_array << letter
-  end
-  other_array.sort!
-  counter = 1
-  the_real_other = []
-  other_array.each do |letter|
-    the_real_other << letter + counter.to_s
-    counter += 1
-    if counter > length
-      counter = 1
+
+  def cells
+    board_hash = {}
+    letter_converter(width, length).each do |coordinate|
+      board_hash[coordinate] = Cell.new(coordinate)
     end
+    board_hash
   end
-  the_real_other
-end
+
+  end
+
+  def letter_converter(width, length)
+    alphabet = ("A".."Z").to_a
+    width_letter_array = (1..width).map do |num|
+      num = alphabet[num - 1]
+    end
+    letter_array = []
+    width_letter_array.cycle(length) do |letter|
+      letter_array << letter
+    end
+    letter_array.sort!
+    counter = 1
+    coordinate_array = []
+    letter_array.each do |letter|
+      coordinate_array << letter + counter.to_s
+      counter += 1
+      if counter > length
+        counter = 1
+      end
+    end
+    coordinate_array
+  end
