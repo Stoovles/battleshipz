6384194b2ab5c287e668053af37c9eb5122e23ca
Erin King <eohone@gmail.com>
2019-02-02 13:29:18 -0700

    added test_it_can_choose_random_cell and choose_random_cell methods

--- battleshipz/lib/computer_player.rb
+++ battleshipz/lib/computer_player.rb
@@ -2,5 +2,18 @@ require 'pry'
 
 class ComputerPlayer
 
-  
+  # def initialize
+  #
+  # end
+
+  def choose_random_cell(board)
+    loop do
+      random_cell = board.cells.values.sample
+        if random_cell.empty?
+          return random_cell
+        end
+    end
+  end
+
+
 end
--- battleshipz/test/computer_player_test.rb
+++ battleshipz/test/computer_player_test.rb
@@ -8,7 +8,18 @@ require './lib/computer_player'
 class ComputerPlayerTest < Minitest::Test
 
   def test_it_exists
+    computer_player = ComputerPlayer.new
 
+    assert_instance_of ComputerPlayer, computer_player
+  end
+
+  def test_it_can_choose_random_cell
+    computer_player = ComputerPlayer.new
+    board = Board.new(4,4)
+    board.board_hash
+    random_cell = computer_player.choose_random_cell(board)
+
+    assert_instance_of Cell, random_cell
   end
 
 end
